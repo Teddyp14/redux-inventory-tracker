c8f99b10b519a137ebfe20854aa3e9ff14741485
Teddy Peterschmidt <t.a.peterschmidt@gmail.com>
2023-12-21 16:35:19 -0800

    update file names

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './components/App.tsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Provider } from 'react-redux';
-import { store } from './redux/store.tsx'
+import { store } from './redux/store.ts'
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
